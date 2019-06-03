acd49b0765b6267d1f8a5274d3795e65d41432f3
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2019-06-03 22:52:26 +0100

    semantified

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useMutation } from 'urql'
 
 import AddCommentForm from './AddCommentForm'
@@ -35,10 +35,12 @@ mutation DeleteComment($id: Int!) {
 `
 
 const Pokemon = ({ pokemon }) => {
+  const [showComments, setShowComments] = useState(false)
   const [, deletePokemon] = useMutation(deletePokemonMutation)
   const [, deleteComment] = useMutation(deleteCommentMutation)
   const [, likePokemon] = useMutation(likePokemonMutation)
 
+  const toggleComments = () => setShowComments(!showComments)
 
   return <div className="ui card">
     <div className="content">
@@ -53,76 +55,31 @@ const Pokemon = ({ pokemon }) => {
         <i onClick={() => likePokemon({ id: pokemon.id })} className="like icon"></i>
         {pokemon.likes} {pokemon.likes === 1 ? 'Like' : 'Likes'}
       </a>
-      <i className="comment icon"></i>
-      {pokemon.comments.length} comments
+      <a onClick={toggleComments}>
+        <i className={`comment ${showComments ? '' : 'outline'} icon`}></i>
+        {pokemon.comments.length} comments
+      </a>
     </div>
-    <div class="ui celled list">
-      {
-        pokemon.comments.map(comment => <div key={`comment-${comment.id}`} className="item">
-          <div className="content">
-            <div className="header">{comment.content}</div>
+    {
+      showComments &&
+        <div className='content'>
+          <div class="ui divided list">
+            {
+              pokemon.comments.map(comment => <div key={`comment-${comment.id}`} className="item">
+                <i className='comment outline icon'></i>
+                <div className="content">
+                  <div className="header">{comment.content}</div>
+                  <i onClick={() => deleteComment({ id: comment.id })} className="trash red icon right floated"></i>
+                </div>
+              </div>)
+            }
           </div>
-        </div>)
-      }
-    </div>
+        </div>
+    }
     <div className="extra content">
       <AddCommentForm id={pokemon.id} />      
     </div>
   </div>
-
-  return <div className="ui card yellow">
-    <div className="image">
-      <img src={pokemon.image} alt={pokemon.name} />
-    </div>
-    <div className="content">
-      <a className="header">{pokemon.name.toUpperCase()}</a>
-      <div className="meta">
-        <span className="date">Gotta catch 'em all</span>
-      </div>
-    </div>
-    <AddCommentForm id={pokemon.id} />
-    <div className="ui middle aligned animated list">
-      {
-        pokemon.comments.map(comment => <div key={`comment-${comment.id}`} className="item">
-          <img className="ui avatar image" src="https://i.pinimg.com/236x/64/2c/2a/642c2aad1cdbc6e3179d3e22330768b3--pokeball-tattoo-pokemon-images.jpg" />
-          <div className="content">
-            <div className="header">{comment.content}</div>
-          </div>
-        </div>)
-      }
-    </div>
-    <div className="extra content">
-      <a>
-        <i onClick={() => likePokemon({ id: pokemon.id })} className="like icon"></i>
-        {pokemon.likes} {pokemon.likes === 1 ? 'Like' : 'Likes'}
-      </a>
-      <a className='right'>
-        <i onClick={() => likePokemon({ id: pokemon.id })} className="delete icon"></i>
-      </a>
-    </div>
-  </div>
-
-  // return <div className='ui card'>
-  //   <p>{pokemon.name}<button onClick={() => deletePokemon({ id: pokemon.id })}>X</button></p>
-  //   <img style={imageStyle} src={pokemon.image} alt={pokemon.name} />
-  //   <p>
-  //     Likes: {pokemon.likes}
-  //     <button onClick={() => likePokemon({ id: pokemon.id })}>LIKE!</button>
-  //   </p>
-  //   <ul>
-  //     {
-  //       pokemon.comments.map(comment =>
-  //         <li
-  //           key={`comment-${comment.id}`}
-  //           onClick={() => deleteComment({ id: comment.id })}
-  //         >
-  //           {comment.content}
-  //         </li>
-  //       )
-  //     }
-      
-  //   </ul>
-  // </div>
 }
 
 export default Pokemon
